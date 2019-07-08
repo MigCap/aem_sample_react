fefa14983da13aa3c3abd4f0c1f19240ef7223cb
Miguel Capellan <miguelangel.capellan@mad.vass.es>
2019-07-08 14:36:29 +0200

    Added API mock connection & pagination to list component.

--- react-app/src/utils/Icons.js
+++ react-app/src/utils/Icons.js
@@ -1,4 +1,26 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckSquare, faChevronLeft, faSearch, faHeadphonesAlt, faMusic, faCamera, faFutbol, faPaintBrush, faTheaterMasks} from '@fortawesome/free-solid-svg-icons';
- 
-library.add(faCheckSquare, faChevronLeft, faSearch, faHeadphonesAlt, faMusic, faCamera, faFutbol, faPaintBrush, faTheaterMasks);
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheckSquare,
+  faChevronLeft,
+  faChevronRight,
+  faSearch,
+  faHeadphonesAlt,
+  faMusic,
+  faCamera,
+  faFutbol,
+  faPaintBrush,
+  faTheaterMasks
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCheckSquare,
+  faChevronLeft,
+  faChevronRight,
+  faSearch,
+  faHeadphonesAlt,
+  faMusic,
+  faCamera,
+  faFutbol,
+  faPaintBrush,
+  faTheaterMasks
+);
